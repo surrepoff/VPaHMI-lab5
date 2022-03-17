d4c7077065325781488c677e4b0bed0f93925ced
surrepoff <alexander.bessonov548@gmail.com>
2022-03-17 15:15:14 +0700

    Добавьте файлы проекта.

---
+++ VPaHMI-lab5/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace VPaHMI_lab5.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
